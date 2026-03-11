64d974b161edf4324f9bc51c271323fad6367851
sistemas <sistemas@routvigo.local>
2026-03-11 11:53:18 +0100

    Arreglado para comenzar desde aquí

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,10 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-with open(os.path.join(BASE_DIR, "config", "settings.json"), "r") as f:
+with open(os.path.join(BASE_DIR, "config", "settings.json"), "r", encoding="utf-8") as f:
     _config = json.load(f)
 
 ARCHIVO_CSV = os.path.join(BASE_DIR, _config["ARCHIVO_CSV"])
+CLIENTES = _config["CLIENTES"]
+USUARIOS = _config["USUARIOS"]
+ESTADOS = _config["ESTADOS"]
